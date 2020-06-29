b62d35685d134702b3f3fe5cf5928eb486fdb28e
rkibart <r.kibart@gmail.com>
2020-06-29 22:44:25 +0200

    dateprojs v0.2.0
    dodana logika sprawdzająca dni ,
    dodany obiekt date do klasy
    dodana statyczna metoda howManyDays licząca odległość w dniach między instancjami klasy,
    zmienino część wyrażeń regex

--- datepro.js
+++ datepro.js
@@ -14,53 +14,57 @@ class DatePro{
                 throw TypeError(" Input date format must be a string.")
             }
         
-            const regexCheckDay = /^[^D]*D{2}[^D]*$/g  //sprawdza czy jest DD w stringu i nie ma innych D
-            const regexCheckMonth = /^[^M]*M{2}[^M]*$/g  //sprawdza czy jest MM w stringu i nie ma innych M
-            const regexCheckYearYY = /^[^Y]*Y{2}[^Y]*$/g  //sprawdza czy jest YY w stringu i nie ma innych Y
-            const regexCheckYearYYYY = /^[^Y]*Y{4}[^Y]*$/g  //sprawdza czy jest YYYY w stringu i nie ma innych Y
+            const regexCheckDay = /^(?:[^D]*D?[^D])*D{2}(?:[^D]D?[^D]*)*$/ //sprawdza czy jest raz DD w stringu
+            const regexCheckMonth = /^(?:[^M]*M?[^M])*M{2}(?:[^M]M?[^M]*)*$/  //sprawdza czy jest raz MM
+            const regexCheckYearYY = /^(?:[^Y]*Y?[^Y])*Y{2}(?:[^Y]Y?[^Y]*)*$/  //sprawdza czy jest raz YY
+            const regexCheckYearYYYY = /^(?:[^Y]*Y?[^Y])*Y{4}(?:[^Y]Y?[^Y]*)*$/  //sprawdza czy jest raz YYYY
             const regexCheckYearY = /[Y]/g  ////sprawdza czy jest Y w stringu 
         
-            if (inputDateFormat.search(regexCheckDay) === -1) {
+            if (!regexCheckDay.test(inputDateFormat)) {
                 throw new Error(`Input day should be formated: DD`)
             }
             
-            if (inputDateFormat.search(regexCheckMonth) === -1) {
+            if (!regexCheckMonth.test(inputDateFormat)) {
                 throw new Error(`Input month should be formated: MM`)
             }
         
-            if ((inputDateFormat.search(regexCheckYearYYYY) === -1) && (inputDateFormat.search(regexCheckYearYY) === -1)) {
-                
-                if (inputDateFormat.search(regexCheckYearY) !== -1) {
-                    throw new Error(`Input year should be formated: YYYY, YY or absence of Y which means actual year`)
-                }
-            }
+            // if (!regexCheckYearYYYY.test(inputDateFormat) && !regexCheckYearYY.test(inputDateFormat)) {               
+            //     throw new Error(`Input year should be formated: YYYY, YY or absence of Y which means actual year`)
+            // }
             
             return true
             
         }
         
         function getDatePro(dateAsString, inputDateFormat) {
+            
+            const regexCheckDD = /D{2}/  
+            const regexCheckMM = /M{2}/
+            const regexCheckYY = /Y{2}/
+            const regexCheckYYYY = /Y{4}/
+            const regexTwoDigits = /\d{2}/
+            const regexFourDigits = /\d{4}/
+            let day, month, year
+            const today = new Date()
+            
             if (typeof dateAsString !== "string") {
                 throw TypeError(`Input date must be a string.`)
+            }
+
+            if(dateAsString.length + 2 === inputDateFormat.length) {  // sytuacja gdy inputDateFormat ma rok YYYY a dateAsString ma YY
+                inputDateFormat = inputDateFormat.replace(regexCheckYY, '')
             }
         
             if (dateAsString.length !== inputDateFormat.length) {
                 throw new Error(`Input date must be the same as inputDateFormat or default: DD.MM.YYYY`)
             }
         
-            const regexCheckDD = /D{2}/g  
-            const regexCheckMM = /M{2}/g
-            const regexCheckYY = /Y{2}/g
-            const regexCheckYYYY = /Y{4}/g
-            const regexTwoDigits = /\d{2}/
-            const regexFourDigits = /\d{4}/
-            const today = new Date()
             
             const dayPosition = inputDateFormat.search(regexCheckDD)
             const dayString =  dateAsString.slice(dayPosition, dayPosition+2)
         
             if (regexTwoDigits.test(dayString)) {  //sprawdzam czy w stringu są 2 cyfry
-                that.day = parseInt(dayString, 10)
+                day = parseInt(dayString, 10)
             } else {
                 throw new Error (`Input date does not match the date pattern`)
             }
@@ -69,7 +73,7 @@ class DatePro{
             const monthString =  dateAsString.slice(monthPosition, monthPosition+2)
             
             if (regexTwoDigits.test(monthString)) {  //sprawdzam czy w stringu są 2 cyfry 
-                that.month = parseInt(monthString, 10)
+                month = parseInt(monthString, 10)
             } else {
                 throw new Error (`Input date does not match the date pattern`)
             }
@@ -80,12 +84,12 @@ class DatePro{
                 yearPosition = inputDateFormat.search(regexCheckYY)
         
                 if (yearPosition === -1 ) { // gdy rok nie jest podany ustawiamy na dzisiejszy
-                    that.year = today.getFullYear()
+                    year = today.getFullYear()
         
                 } else {  // gdy rok jest sformatowany YY
                     const yearString = dateAsString.slice(yearPosition, yearPosition + 2)
                     if (regexTwoDigits.test(yearString)) {  // sprawdzam czy w tringu są 2 cyfry
-                        that.year = parseInt(yearString, 10) + 100*Math.floor(today.getFullYear() / 100)
+                        year = parseInt(yearString, 10) + 100*Math.floor(today.getFullYear() / 100)
                     } else {
                         throw new Error (`Input date does not match the date pattern`)
                     }
@@ -94,11 +98,24 @@ class DatePro{
             } else {    // gdy rok jest sformatowany YYYY
                 const yearString = dateAsString.slice(yearPosition, yearPosition + 4)
                 if (regexFourDigits.test(yearString)) {  // sprawdzam czy w tringu są 4 cyfry
-                    that.year = parseInt(yearString, 10)
+                    year = parseInt(yearString, 10)
                 } else {
                     throw new Error (`Input date does not match the date pattern`)
                 }
             }
+
+            // sprawdzamy czy dany dzień istnieje
+            const daysInfebruary =  (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) ? 29 : 28   // jeśli rok przestępny to 29
+            const daysInMonth = [31, daysInfebruary, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+            if ( day === 0 || month === 0 || month > 12 || day > daysInMonth[month - 1]) {
+                throw new Error (`Day ${day} in month ${month} in year ${year} does not exist!`)
+            }
+
+            that.day = day
+            that.month = month
+            that.year = year
+            that.date = new Date(year, month - 1, day)
             
         }
 
@@ -124,8 +141,7 @@ class DatePro{
             if(!regexCheckAll.test(outputStringFormat)) {
                     throw new Error(`OutputStringFormat must contain at least one of DD MM YY YYYY`)
             }
-            
-            
+                       
             return true
             
         }
@@ -146,17 +162,19 @@ class DatePro{
                 outputString = outputString.replace(regexCheckYY, year.toString().slice(2))
         
             }
-        
-        
+                
             return outputString
-            
-        
+                   
         }
 
         // wywolujemy funkcje
         if (validationOutputDateFormat(outputStringFormat)) {
            return formatOutput(day, month, year, outputStringFormat)
         }
+    }
+
+    static howManyDays(date1, date2) {
+        return Math.abs(Math.floor((date1.date.getTime() - date2.date.getTime()) / (1000 * 60 * 60 * 24)))
     }
 }
 
@@ -176,11 +194,16 @@ const formatDate2 = 'MM/DD/YY'
 const instance2 = new DatePro(date2, formatDate2)
 
 
-// const date3 = '20-03-20'
-// const formatDate3 = 'DD-MM-YYYY'
-// const instance3 = new DatePro(date3, formatDate3)
+const date3 = '20-03-20'
+const formatDate3 = 'DD-MM-YYYY'
+const instance3 = new DatePro(date3, formatDate3)
+
+const date4 = '29-03-2020'
+const formatDate4 = 'DD-MM-YYYY'
+const instance4 = new DatePro(date4, formatDate4)
 
 
 instance1.format() // '23.03.2020'
 instance2.format() // '23.03.2020'
-//instance3.format() // '23.03.2020'
+instance3.format() // '23.03.2020'
+instance4.format() // '29.03.2020'
